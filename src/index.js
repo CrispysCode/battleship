48b8d753bad8631711f4bf4d3d3356a6921ef4aa
CrispysCode <pr0ject0dinx@gmail.com>
2025-03-14 16:35:53 -0500

    We have both board pre rendered you must click start to show predetermined ships

--- src/index.js
+++ src/index.js
@@ -8,16 +8,35 @@ const {
 const GameSetup = require("./gameSetup-04/gameSetup.js");
 const RenderGame = require("./renderModule.js");
 const GameFlow = require("./gameFlow.js");
+
 import "./styles.css";
 
-document.addEventListener("DOMContentLoaded", () => {
- const startBtn = document.querySelector(".startBtn");
- startBtn.addEventListener("click", () => {
-  const gameSetup = new GameSetup();
-  gameSetup.startGame();
+const startBtn = document.querySelector(".startBtn");
+const userBoard = document.querySelector(".userBoard");
+const computerBoard = document.querySelector(".computerBoard");
 
-  const gameFlow = new GameFlow(gameSetup);
-  gameFlow.renderBoards();
- 
- })
+
+document.addEventListener("DOMContentLoaded", () => {
+
+ for (let i = 0; i < 100; i++) {
+  const userCell = document.createElement("div");
+  const computerCell = document.createElement("div");
+  userCell.classList.add("cell");
+  userCell.dataset.x = Math.floor(i / 10);
+  userCell.dataset.y = i % 10;
+  userBoard.appendChild(userCell);
+  computerCell.classList.add("cell");
+  computerCell.dataset.x = Math.floor(i / 10);
+  computerCell.dataset.y = i % 10;
+  computerBoard.appendChild(computerCell);
+ }
+
+})
+startBtn.addEventListener("click", () => {
+ const gameSetup = new GameSetup();
+ gameSetup.startGame();
+
+ const gameFlow = new GameFlow(gameSetup);
+ gameFlow.renderBoards();
+
 })
